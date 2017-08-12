49554d1d76920f0363fbb37193888fd992c69950
Welfare <rwm810@gmail.com>
2017-08-11 22:16:48 -0400

    Camera
    Camera Follow player  with offset added

---
+++ Assets/Block.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+
+    public float blockSpeed = 15f;
+    public Rigidbody rig;
+
+    void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate ()
+    {
+        rig.AddForce(-transform.forward * blockSpeed );
+	}
+
+}
