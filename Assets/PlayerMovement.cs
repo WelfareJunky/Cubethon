253d6d289fe7c7752cb434d09ff8439398e75809
Welfare <rwm810@gmail.com>
2017-08-11 21:08:47 -0400

    Movement and Spawn points added
    Horizontal movement added spawnpoints added
    no force added yet

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 15f;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal") * Time.fixedDeltaTime * speed;
+
+        rb.MovePosition( rb.position + Vector3.right * x);
+
+
+    }
+
+}
